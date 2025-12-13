aa669c39258624c8c5fb4399caf22106069c4233
rotoon <rotoon.np@gmail.com>
2025-12-13 15:12:04 +0700

    promise

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import {
   getEventById,
   addEvent,
 } from './services/EventService'
+import type Event from './models/Event'
 
 const app = express()
 const port = 3000
@@ -14,43 +15,32 @@ app.use(express.json())
 app.get('/events', (req, res) => {
   if (req.query.category) {
     const category = req.query.category as string
-    const filteredEvents = getEventByCategory(category)
-    res.json(filteredEvents)
-  } else if (req.query.title) {
-    const title = req.query.title as string
-    const filteredEvents = getAllEvents().filter((event) =>
-      event.title.startsWith(title)
-    )
-    res.json(filteredEvents)
+    getEventByCategory(category).then((events: Event[]) => {
+      res.json(events)
+    })
   } else {
-    res.json(getAllEvents())
+    getAllEvents().then((events) => {
+      res.send(events)
+    })
   }
 })
 
 app.get('/events/:id', (req, res) => {
   const id = parseInt(req.params.id)
-  const event = getEventById(id)
-  if (event) {
-    res.json(event)
-  } else {
-    res.status(404).send('Event not found')
-  }
+  getEventById(id).then((event: Event | undefined) => {
+    if (event) {
+      res.json(event)
+    } else {
+      res.status(404).send('Event not found')
+    }
+  })
 })
 
 app.post('/events', (req, res) => {
-  const newEvent = req.body
-  if (newEvent.id) {
-    const existingIndex = getAllEvents().findIndex(
-      (event) => event.id === newEvent.id
-    )
-    if (existingIndex !== -1) {
-      getAllEvents()[existingIndex] = newEvent
-      res.json(newEvent)
-      return
-    }
-  }
-  addEvent(newEvent)
-  res.json(newEvent)
+  const newEvent: Event = req.body
+  addEvent(newEvent).then((event: Event) => {
+    res.json(event)
+  })
 })
 
 app.listen(port, () => {
